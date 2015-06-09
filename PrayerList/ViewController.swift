3a476b18c1ada27cda007517436bdcb86ebb624f
Phil Miller <ptfmiller@gmail.com>
2015-06-08 22:52:23 -0400

    Successfully removed the duplicate (useless) main.storyboard. Next task is to get the storyboard and the app working as it was before. Then I will move on to integrating Parse.

--- PrayerList/ViewController.swift
+++ PrayerList/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        println("I can't wait to push this project to github")
     }
 
     override func didReceiveMemoryWarning() {
